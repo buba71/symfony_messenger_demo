18e4f7a94ce7af410ec8f6d16d86edec6a6aa7da
De Lima David <buba71@sfr.fr>
2019-01-10 23:11:21 +0100

    messenger ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Message\SendNotification;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Twig\Environment;
@@ -11,9 +11,12 @@ use Twig\Environment;
 
 class HomeController
 {
-    public function index(Environment $twig, MessageBusInterface $bus): Response
+    public function index(Request $request, Environment $twig, MessageBusInterface $bus): Response
     {
-        $bus->dispatch(new SendNotification('Hello World !', ['email_1@example.com', 'email_2@example.com' ]));
+        $message = $request->query->get('message', 'Hello worl!');
+        $recipients =  ['email_1@example.com', 'email_2@example.com' ];
+
+        $bus->dispatch(new SendNotification($message, $recipients));
 
         return new Response($twig->render('home/index.html.twig'));
 
--- src/MessageHandler/SendNotificationHandler.php
+++ src/MessageHandler/SendNotificationHandler.php
@@ -3,13 +3,36 @@
 namespace App\MessageHandler;
 
 use App\Message\SendNotification;
+use Swift_Mailer;
 
 class SendNotificationHandler
 {
+    private $mailer;
+    public function __construct(Swift_Mailer $mailer)
+    {
+
+        $this->mailer =$mailer;
+    }
     public function __invoke(SendNotification $notification)
     {
+        //sleep(10);
+
         foreach ($notification->getRecipients() as $recipient){
-            echo sprintf('Envoi du message "%s" à [%s]', $notification->getMessage(), $recipient). '<br/>';
+
+            $message = (new \Swift_Message('[Notification]'))
+                ->setFrom('d.delima@example.com')
+                ->setTo($recipient)
+                ->setBody(
+                    sprintf('<h1>Notification</h1><p>%s</p>', $notification->getMessage())
+                    ,
+                    'text/html'
+                )
+
+            ;
+
+            $this->mailer->send($message);
+
+            dump(sprintf('Envoi du message "%s" à [%s]', $notification->getMessage(), $recipient). '<br/>');
         }
     }
 }
